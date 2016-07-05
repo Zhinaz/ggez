628295d4bbc4c96db5b7f588a790d8a3debc54e1
Victor Hvass <Victor Hvass>
2016-07-05 15:13:13 +0200

    kartoffelhovedmand

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
 			game.load.image('sky', 'assets/sky.png');
 		    game.load.image('ground', 'assets/platform.png');
 		    game.load.image('star', 'assets/star.png');
-		    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+		    game.load.spritesheet('dude', 'assets/kartoffelhovedmand.png', 32, 48);
 		}
 
 		var platforms;
